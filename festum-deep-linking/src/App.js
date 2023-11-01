8b9f1aaa88ab43bc4fb93824aefcfee2f65873f1
joao gabriel <jg.7651@gmail.com>
2023-11-01 15:58:00 -0300

    primeira versao definitiva

--- festum-deep-linking/src/App.js
+++ festum-deep-linking/src/App.js
@@ -1,12 +1,32 @@
 
+import { useEffect } from 'react';
 import './App.css';
+
+
 import logo from './assets/images/logo.png'
 
 function App() {
+  useEffect(() => {
+    setTimeout(()=> {
+      const path = window.location.pathname.replace('/', '');
+      window.location.replace('festum://' + path)
+    }, 25)
+    
+  },[])
   return (
     <div className="App">
-      <h1>FESTUM APP</h1>
-      <img src={logo} alt="text"/>
+      <div className='main-background'>
+        <div className='main-text-wrap'>
+          
+          <img className='logo-text-wrap' src={logo} alt='logo'/>
+          <p className='main-text'>Baixe o aplicativo usando o botão abaixo!</p>
+          <div >
+            <a className='google-button' href='https://play.google.com/store/apps/details?id=com.gabriel761.frontend&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+              <img className='google-button-img' alt='Disponível no Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/pt-br_badge_web_generic.png' />
+              </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
